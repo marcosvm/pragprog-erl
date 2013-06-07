0ebae04f719f4033e6205ca60e376342de02cbfa
Marcos Oliveira <marcosvm@gmail.com>
2013-06-07 09:26:33 -0700

    Chapter 2, task 1, refactoring chat_client.
      chat_client is refactored to be displaying the message.
    
      We create the print_message function and pass a reference
      of it to message_router using the fun keyword.
      Also chat_client has a start_router function.
    
      Extra: fix a tail recursion bug on send_chat_msg, we
      weren't calling ourselves.

--- chat_client.erl
+++ chat_client.erl
@@ -1,6 +1,12 @@
 -module(chat_client).
 
--export([send_chat_message/3]).
+-export([send_message/3, print_message/1, start_router/0]).
 
-send_chat_message(RouterPid, Addressee, MessageBody) ->
+send_message(RouterPid, Addressee, MessageBody) ->
   message_router:send_message(RouterPid, Addressee, MessageBody).
+
+print_message(MessageBody) ->
+  io:format("Received: ~p~n", [MessageBody]).
+
+start_router() ->
+  message_router:start(fun chat_client:print_message/1).
--- message_router.erl
+++ message_router.erl
@@ -3,10 +3,10 @@
 
 -module(message_router).
 
--export([send_message/3,route_messages/0, start/0, stop/1]).
+-export([send_message/3,route_messages/1, start/1, stop/1]).
 
-start() ->
-  spawn(message_router, route_messages, []).
+start(PrintFun) ->
+  spawn(message_router, route_messages, [PrintFun]).
 
 stop(RouterPid) ->
   RouterPid ! shutdown.
@@ -14,15 +14,16 @@ stop(RouterPid) ->
 send_message(RouterPid, Addressee, MessageBody) ->
   RouterPid ! {send_chat_msg, Addressee, MessageBody}.
 
-route_messages() ->
+route_messages(PrintFun) ->
   receive
     {send_chat_msg, Addressee, MessageBody} ->
-      Addressee ! {recv_chat_msg, MessageBody};
+      Addressee ! {recv_chat_msg, MessageBody},
+      route_messages(PrintFun);
     {recv_chat_msg, MessageBody} ->
-      io:format("Received: ~p~n", [MessageBody]);
+      PrintFun(MessageBody);
     shutdown ->
       io:format("Shutting down~n");
     Oops ->
       io:format("Warning! Received: ~p~n", [Oops]),
-      route_messages()
+      route_messages(PrintFun)
   end.
